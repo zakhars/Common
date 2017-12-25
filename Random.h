e9bc76f852febabd181e4b85afa91ae528f961ef
Zakhar Stavitsky <zakhar.stavitsky@gmail.com>
2017-12-25 23:27:26 +0300

    Current progress

--- Random.h
+++ Random.h
@@ -8,20 +8,19 @@ namespace common
 
    class rand_int
    {
-      std::random_device rd;
-      std::uniform_int_distribution<int> dis;
-      std::default_random_engine eng;
+      std::uniform_int_distribution<int> distribution;
+      std::default_random_engine engine;
       int min_val;
       int max_val;
 
    public:
       rand_int(int min_val = 0, int max_val = std::numeric_limits<int>::max()) : 
-         min_val {min_val}, max_val {max_val}, dis{min_val, max_val}
+         min_val {min_val}, max_val {max_val}, distribution{min_val, max_val}
       {}
 
       int operator()()
       {
-         return dis(eng);
+         return distribution(engine);
       }
    };
 }
--- Timer.h
+++ Timer.h
@@ -11,12 +11,17 @@ class timer
 {
    std::chrono::high_resolution_clock::time_point t0;
 public:
-   timer() { save_time(); }
-   void save_time() { t0 = std::chrono::high_resolution_clock::now(); }
-   int64_t get_duration_us()
+   timer() { spot(); }
+   void spot() { t0 = std::chrono::high_resolution_clock::now(); }
+   int64_t us()
    { 
       auto t1 = std::chrono::high_resolution_clock::now(); 
       return std::chrono::duration_cast<microseconds>(t1-t0).count();
+   }
+   int64_t ns()
+   { 
+      auto t1 = std::chrono::high_resolution_clock::now(); 
+      return std::chrono::duration_cast<nanoseconds>(t1-t0).count();
    }
 };
 
